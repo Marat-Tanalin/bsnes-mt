f1dc1386e446e6c40e40414f3290691b90ee831e
Marat Tanalin <Marat-Tanalin@users.noreply.github.com>
2020-10-07 20:49:50 +0300

    target-bsnes/presentation/presentation.hpp: Fixed indentation

--- bsnes/target-bsnes/presentation/presentation.hpp
+++ bsnes/target-bsnes/presentation/presentation.hpp
@@ -39,103 +39,103 @@ struct Presentation : Window {
 		Menu controllerPort1{&systemMenu};
 		Menu controllerPort2{&systemMenu};
 		Menu expansionPort{&systemMenu};
-		Menu settingsMenu{&menuBar};
-			Menu sizeMenu{&settingsMenu};
-				MenuItem fullScreenMenuItem; // MT.
-				MenuItem pseudoFullScreenMenuItem; // MT.
-			Group sizeGroup;
-			Menu outputMenu{&settingsMenu};
-				MenuRadioItem centerViewport{&outputMenu};
-				MenuRadioItem perfectViewport{&outputMenu}; // MT.
-				MenuRadioItem scaleViewport{&outputMenu};
-				MenuRadioItem stretchViewport{&outputMenu};
-				Group outputGroup{&centerViewport, &perfectViewport, &scaleViewport, &stretchViewport}; // `&perfectViewport,` added by MT.
-				MenuSeparator outputSeparator{&outputMenu};
-				MenuCheckItem aspectCorrection{&outputMenu};
-				MenuCheckItem parInsteadOfAr{&outputMenu};
-				MenuCheckItem showOverscanArea{&outputMenu};
-				MenuCheckItem scalingInfo{&outputMenu}; // MT.
-				MenuCheckItem blurEmulation{&outputMenu};
-			Menu filterMenu{&settingsMenu};
-				MenuRadioItem filterNone{&filterMenu};
-				MenuRadioItem filterScanlinesLight{&filterMenu};
-				MenuRadioItem filterScanlinesDark{&filterMenu};
-				MenuRadioItem filterScanlinesBlack{&filterMenu};
-				MenuRadioItem filterPixellate2x{&filterMenu};
-				MenuRadioItem filterScale2x{&filterMenu};
-				MenuRadioItem filter2xSaI{&filterMenu};
-				MenuRadioItem filterSuper2xSaI{&filterMenu};
-				MenuRadioItem filterSuperEagle{&filterMenu};
-				MenuRadioItem filterLQ2x{&filterMenu};
-				MenuRadioItem filterHQ2x{&filterMenu};
-				MenuRadioItem filterNTSC_RF{&filterMenu};
-				MenuRadioItem filterNTSC_Composite{&filterMenu};
-				MenuRadioItem filterNTSC_SVideo{&filterMenu};
-				MenuRadioItem filterNTSC_RGB{&filterMenu};
-				Group filterGroup{
-					&filterNone,
-					&filterScanlinesLight,
-					&filterScanlinesDark,
-					&filterScanlinesBlack,
-					&filterPixellate2x,
-					&filterScale2x,
-					&filter2xSaI,
-					&filterSuper2xSaI,
-					&filterSuperEagle,
-					&filterLQ2x,
-					&filterHQ2x,
-					&filterNTSC_RF,
-					&filterNTSC_Composite,
-					&filterNTSC_SVideo,
-					&filterNTSC_RGB
-				};
-			Menu shaderMenu{&settingsMenu};
-			MenuSeparator settingsSeparatorA{&settingsMenu};
-			MenuCheckItem muteAudio{&settingsMenu};
-			MenuCheckItem showStatusBar{&settingsMenu};
-			MenuSeparator settingsSeparatorB{&settingsMenu};
-			MenuItem videoSettings{&settingsMenu};
-			MenuItem audioSettings{&settingsMenu};
-			MenuItem inputSettings{&settingsMenu};
-			MenuItem hotkeySettings{&settingsMenu};
-			MenuItem builtinHotkeysSettings{&settingsMenu}; // MT.
-			MenuItem pathSettings{&settingsMenu};
-			MenuItem emulatorSettings{&settingsMenu};
-			MenuItem enhancementSettings{&settingsMenu};
-			MenuItem compatibilitySettings{&settingsMenu};
-			MenuItem driverSettings{&settingsMenu};
-			MenuItem settingsFolder{&settingsMenu}; // MT.
-		Menu toolsMenu{&menuBar};
-			Menu saveState{&toolsMenu};
-			Menu loadState{&toolsMenu};
-			MenuSeparator toolsSeparatorB{&toolsMenu};
-			Menu speedMenu{&toolsMenu};
-				MenuRadioItem speedSlowest{&speedMenu};
-				MenuRadioItem speedSlow{&speedMenu};
-				MenuRadioItem speedNormal{&speedMenu};
-				MenuRadioItem speedFast{&speedMenu};
-				MenuRadioItem speedFastest{&speedMenu};
-				Group speedGroup{&speedSlowest, &speedSlow, &speedNormal, &speedFast, &speedFastest};
-			Menu runMenu{&toolsMenu};
-				MenuRadioItem runEmulation{&runMenu};
-				MenuRadioItem pauseEmulation{&runMenu};
-				MenuRadioItem frameAdvance{&runMenu};
-				Group runGroup{&runEmulation, &pauseEmulation, &frameAdvance};
-			Menu movieMenu{&toolsMenu};
-				MenuItem moviePlay{&movieMenu};
-				MenuItem movieRecord{&movieMenu};
-				MenuItem movieRecordFromBeginning{&movieMenu};
-				MenuItem movieStop{&movieMenu};
-			MenuItem captureScreenshot{&toolsMenu};
-			MenuSeparator toolsSeparatorC{&toolsMenu};
-			MenuItem cheatFinder{&toolsMenu};
-			MenuItem cheatEditor{&toolsMenu};
-			MenuItem stateManager{&toolsMenu};
-			MenuItem manifestViewer{&toolsMenu};
-		Menu helpMenu{&menuBar};
-			MenuItem aboutSameBoy{&helpMenu};
-			MenuItem aboutBsnes{&helpMenu};
-			MenuItem aboutBsnesMt{&helpMenu}; // MT.
+	Menu settingsMenu{&menuBar};
+		Menu sizeMenu{&settingsMenu};
+			MenuItem fullScreenMenuItem; // MT.
+			MenuItem pseudoFullScreenMenuItem; // MT.
+		Group sizeGroup;
+		Menu outputMenu{&settingsMenu};
+			MenuRadioItem centerViewport{&outputMenu};
+			MenuRadioItem perfectViewport{&outputMenu}; // MT.
+			MenuRadioItem scaleViewport{&outputMenu};
+			MenuRadioItem stretchViewport{&outputMenu};
+			Group outputGroup{&centerViewport, &perfectViewport, &scaleViewport, &stretchViewport}; // `&perfectViewport,` added by MT.
+			MenuSeparator outputSeparator{&outputMenu};
+			MenuCheckItem aspectCorrection{&outputMenu};
+			MenuCheckItem parInsteadOfAr{&outputMenu};
+			MenuCheckItem showOverscanArea{&outputMenu};
+			MenuCheckItem scalingInfo{&outputMenu}; // MT.
+			MenuCheckItem blurEmulation{&outputMenu};
+		Menu filterMenu{&settingsMenu};
+			MenuRadioItem filterNone{&filterMenu};
+			MenuRadioItem filterScanlinesLight{&filterMenu};
+			MenuRadioItem filterScanlinesDark{&filterMenu};
+			MenuRadioItem filterScanlinesBlack{&filterMenu};
+			MenuRadioItem filterPixellate2x{&filterMenu};
+			MenuRadioItem filterScale2x{&filterMenu};
+			MenuRadioItem filter2xSaI{&filterMenu};
+			MenuRadioItem filterSuper2xSaI{&filterMenu};
+			MenuRadioItem filterSuperEagle{&filterMenu};
+			MenuRadioItem filterLQ2x{&filterMenu};
+			MenuRadioItem filterHQ2x{&filterMenu};
+			MenuRadioItem filterNTSC_RF{&filterMenu};
+			MenuRadioItem filterNTSC_Composite{&filterMenu};
+			MenuRadioItem filterNTSC_SVideo{&filterMenu};
+			MenuRadioItem filterNTSC_RGB{&filterMenu};
+			Group filterGroup{
+				&filterNone,
+				&filterScanlinesLight,
+				&filterScanlinesDark,
+				&filterScanlinesBlack,
+				&filterPixellate2x,
+				&filterScale2x,
+				&filter2xSaI,
+				&filterSuper2xSaI,
+				&filterSuperEagle,
+				&filterLQ2x,
+				&filterHQ2x,
+				&filterNTSC_RF,
+				&filterNTSC_Composite,
+				&filterNTSC_SVideo,
+				&filterNTSC_RGB
+			};
+		Menu shaderMenu{&settingsMenu};
+		MenuSeparator settingsSeparatorA{&settingsMenu};
+		MenuCheckItem muteAudio{&settingsMenu};
+		MenuCheckItem showStatusBar{&settingsMenu};
+		MenuSeparator settingsSeparatorB{&settingsMenu};
+		MenuItem videoSettings{&settingsMenu};
+		MenuItem audioSettings{&settingsMenu};
+		MenuItem inputSettings{&settingsMenu};
+		MenuItem hotkeySettings{&settingsMenu};
+		MenuItem builtinHotkeysSettings{&settingsMenu}; // MT.
+		MenuItem pathSettings{&settingsMenu};
+		MenuItem emulatorSettings{&settingsMenu};
+		MenuItem enhancementSettings{&settingsMenu};
+		MenuItem compatibilitySettings{&settingsMenu};
+		MenuItem driverSettings{&settingsMenu};
+		MenuItem settingsFolder{&settingsMenu}; // MT.
+	Menu toolsMenu{&menuBar};
+		Menu saveState{&toolsMenu};
+		Menu loadState{&toolsMenu};
+		MenuSeparator toolsSeparatorB{&toolsMenu};
+		Menu speedMenu{&toolsMenu};
+			MenuRadioItem speedSlowest{&speedMenu};
+			MenuRadioItem speedSlow{&speedMenu};
+			MenuRadioItem speedNormal{&speedMenu};
+			MenuRadioItem speedFast{&speedMenu};
+			MenuRadioItem speedFastest{&speedMenu};
+			Group speedGroup{&speedSlowest, &speedSlow, &speedNormal, &speedFast, &speedFastest};
+		Menu runMenu{&toolsMenu};
+			MenuRadioItem runEmulation{&runMenu};
+			MenuRadioItem pauseEmulation{&runMenu};
+			MenuRadioItem frameAdvance{&runMenu};
+			Group runGroup{&runEmulation, &pauseEmulation, &frameAdvance};
+		Menu movieMenu{&toolsMenu};
+			MenuItem moviePlay{&movieMenu};
+			MenuItem movieRecord{&movieMenu};
+			MenuItem movieRecordFromBeginning{&movieMenu};
+			MenuItem movieStop{&movieMenu};
+		MenuItem captureScreenshot{&toolsMenu};
+		MenuSeparator toolsSeparatorC{&toolsMenu};
+		MenuItem cheatFinder{&toolsMenu};
+		MenuItem cheatEditor{&toolsMenu};
+		MenuItem stateManager{&toolsMenu};
+		MenuItem manifestViewer{&toolsMenu};
+	Menu helpMenu{&menuBar};
+		MenuItem aboutSameBoy{&helpMenu};
+		MenuItem aboutBsnes{&helpMenu};
+		MenuItem aboutBsnesMt{&helpMenu}; // MT.
 
 	VerticalLayout layout{this};
 		HorizontalLayout viewportLayout{&layout, Size{~0, ~0}, 0};
